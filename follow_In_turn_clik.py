fc61a35ade94cd54cde0ef44652c22122e0732d5
kimi5231 <djwjod@naver.com>
2023-10-01 02:11:45 +0900

    make arrow mouse motion and draw arrow

--- follow_In_turn_clik.py
+++ follow_In_turn_clik.py
@@ -9,6 +9,7 @@ arrow = load_image('hand_arrow.png')
 
 def handle_events():
     global running
+    global arrowX, arrowY
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -16,14 +17,20 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_ESCAPE:
                 running = False
+        elif event.type == SDL_MOUSEMOTION:
+            arrowX = event.x
+            arrowY = HEIGHT - 1 - event.y
 
 running = True
 frameX, frameY = 0, 3
 boyX, boyY = WIDTH//2, HEIGHT//2
+arrowX, arrowY = 0, 0
 
+hide_cursor()
 while(running):
     clear_canvas()
     background.draw(WIDTH // 2, HEIGHT // 2)
+    arrow.draw(arrowX, arrowY)
     character.clip_draw(frameX * 100, frameY * 100, 100, 100, boyX, boyY)
     update_canvas()
     handle_events()
