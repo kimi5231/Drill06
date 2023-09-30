376f4ef73d4a440b3d574a154229bf158b377d32
kimi5231 <djwjod@naver.com>
2023-10-01 01:43:29 +0900

    make handle_events func

--- follow_In_turn_clik.py
+++ follow_In_turn_clik.py
@@ -7,4 +7,19 @@ character = load_image('animation_sheet.png')
 background = load_image('TUK_GROUND.png')
 arrow = load_image('hand_arrow.png')
 
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                running = False
+
+running = True
+
+while(running):
+    handle_events()
+
 close_canvas()
