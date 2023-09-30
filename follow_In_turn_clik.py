61bb9cfbbb8017d0d8343348c78174a7cabe9189
kimi5231 <djwjod@naver.com>
2023-10-01 04:14:48 +0900

    added follow_hand_arrow func
    +make move variable (check exist hand arrow)
    
    when, hand arrow exist, move true
    when, hand arrow not exist, move false

--- follow_In_turn_clik.py
+++ follow_In_turn_clik.py
@@ -16,6 +16,7 @@ def handle_events():
     global running
     global arrowX, arrowY
     global arrowPos
+    global move
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -30,10 +31,24 @@ def handle_events():
             if event.button == SDL_BUTTON_LEFT:
                 arrowPos.append(Arrow(event.x, HEIGHT - 1 - event.y))
 
+def follow_hand_arrow():
+    global boyX, boyY
+    global j
+    global move
+    t = j/100
+    boyX = (1 - t) * boyX + t * arrowPos[0].x
+    boyY = (1 - t) * boyY + t * arrowPos[0].y
+    if boyX == arrowPos[0].x and boyY == arrowPos[0].y:
+        arrowPos.pop(0)
+        move = False
+        j = 0
+
 running = True
+move = False
 frameX, frameY = 0, 3
 boyX, boyY = WIDTH//2, HEIGHT//2
 arrowX, arrowY = 0, 0
+j = 0
 
 arrowPos = []
 
@@ -45,6 +60,10 @@ while(running):
     character.clip_draw(frameX * 100, frameY * 100, 100, 100, boyX, boyY)
     for i in range(len(arrowPos)):
         arrow.draw(arrowPos[i].x, arrowPos[i].y)
+        move = True
+    if(move):
+        follow_hand_arrow()
+        j += 0.1
     update_canvas()
     handle_events()
 
