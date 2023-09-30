38bba19b1e31b1f8d22053e88d73743902faeb9e
kimi5231 <djwjod@naver.com>
2023-10-01 01:41:20 +0900

    load image

--- follow_In_turn_clik.py
+++ follow_In_turn_clik.py
@@ -1,1 +1,10 @@
 from pico2d import*
+
+WIDTH, HEIGHT = 1100, 600
+open_canvas(WIDTH, HEIGHT)
+
+character = load_image('animation_sheet.png')
+background = load_image('TUK_GROUND.png')
+arrow = load_image('hand_arrow.png')
+
+close_canvas()
